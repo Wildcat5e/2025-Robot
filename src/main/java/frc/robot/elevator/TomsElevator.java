06200b692b63ba91ac1adc03bcad7357ecc5d6c4
Tom Gagnier <tomgagnier@gmail.com>
2025-01-13 17:46:21 -0500

    Example of Elevator using PID

---
+++ src/main/java/frc/robot/elevator/TomsElevator.java
@@ -0,0 +1,38 @@
+
+package frc.robot.elevator;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TomsElevator extends SubsystemBase {
+    public static final double INCHES_PER_ENCODER_TIC = 1.0; // determine through experiment
+    public static final double TOLERANCE_INCHES = 0.20;
+    public static final double MAX_ELEVATOR_SPEED = 1.0;
+
+    private final PIDController pidController;
+    private final TalonFX motor;
+
+    public TomsElevator(PIDController pidController, TalonFX motor) {
+        this.pidController = pidController;
+        this.motor = motor;
+        this.pidController.setTolerance(TOLERANCE_INCHES);
+    }
+
+    public TomsElevator() {
+        this(new PIDController(0.1, 0.0, 0.0), new TalonFX(0));
+    }
+
+    @Override
+    public void periodic() {
+        double currentHeight = motor.getPosition().getValueAsDouble() * INCHES_PER_ENCODER_TIC;
+        double speed = pidController.calculate(currentHeight);
+        double clamped_speed = Math.max(-MAX_ELEVATOR_SPEED, Math.min(MAX_ELEVATOR_SPEED, speed));
+        motor.set(clamped_speed);
+    }
+
+    public void moveTo(double desiredHeightInches) {
+        pidController.setSetpoint(desiredHeightInches);
+    }
+
+}
