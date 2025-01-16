5e7cf099771848d6fdc4430b0fbe5961276930bf
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2025-01-16 18:57:35 -0500

    last save

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,31 +2,33 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.elevator.Elevator;
-import frc.robot.elevator.MoveToPosition;
+import frc.robot.commands.MoveElevatorToPosition;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.ClimbingHooks;
 
 public class RobotContainer {
-  private static final int LEVEL_ONE = 1;
-  private static final int LEVEL_TWO = 2;
-  Elevator elevator = new Elevator();
   XboxController controller = new XboxController(0);
   Trigger ButtonA = new Trigger(() -> controller.getAButton());
   Trigger ButtonB = new Trigger(() -> controller.getBButton());
   Trigger ButtonX = new Trigger(() -> controller.getXButton());
   Trigger ButtonY = new Trigger(() -> controller.getYButton());
+  Elevator elevator = new Elevator();
+  ClimbingHooks climbingHooks = new ClimbingHooks();
 
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    ButtonA.onTrue(new MoveToPosition(elevator, LEVEL_ONE));
-    ButtonB.onTrue(new MoveToPosition(elevator, LEVEL_TWO));
+    ButtonA.onTrue(new MoveElevatorToPosition(elevator, (Double) null));
+
+    ButtonB.onTrue(climbingHooks.extendSolenoid(true));
+
+    ButtonX.onTrue(climbingHooks.extendSolenoid(false));
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return null;
   }
 }
