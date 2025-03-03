957495f1f4dd1cf03d7da00f9423bb298259111e
wildcat6705 <147210783+wildcat6705@users.noreply.github.com>
2025-03-02 19:59:06 -0500

    Add drivetrain code and update wpilib

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,83 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.ElevatorBangBangControl;
-import frc.robot.subsystems.ElevatorFalconPID;
-import frc.robot.subsystems.ElevatorRioPID;
-import frc.robot.subsystems.Limelight;
-import frc.robot.subsystems.Outtake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private final XboxController controller = new XboxController(0);
-  private final Joystick joystick = new Joystick(1);
-  private final JoystickButton button1 = new JoystickButton(joystick, 1);
-  private final JoystickButton button2 = new JoystickButton(joystick, 2);
-  private final JoystickButton button3 = new JoystickButton(joystick, 3);
-  private final JoystickButton button4 = new JoystickButton(joystick, 4);
-  private final JoystickButton button5 = new JoystickButton(joystick, 5);
-  private final JoystickButton button6 = new JoystickButton(joystick, 6);
-  private final JoystickButton button7 = new JoystickButton(joystick, 7);
-  private final JoystickButton button8 = new JoystickButton(joystick, 8);
-  private final JoystickButton button9 = new JoystickButton(joystick, 9);
-  private final JoystickButton button10 = new JoystickButton(joystick, 10);
-  private final JoystickButton button11 = new JoystickButton(joystick, 11);
-  private final JoystickButton button12 = new JoystickButton(joystick, 12);
-  private final Elevator elevator = new ElevatorFalconPID();
-  private final Outtake outtake = new Outtake();
-  private final Limelight limelight = new Limelight();
-  private final Superstructure superstructure = new Superstructure(elevator, outtake, limelight);
   private Command m_autonomousCommand;
 
-  public Robot() {
-    configureBindings();
-  }
+  private final RobotContainer m_robotContainer;
 
-  private void configureBindings() {
-    button1.onTrue(limelight.alignLeft());
-    button3.onTrue(limelight.alignRight());
-    button11.onTrue(superstructure.moveElevatorToLevelOneAndOuttake());
-    button8.onTrue(superstructure.moveElevatorToLevelTwoAndOuttake());
-    button5.onTrue(superstructure.moveElevatorToLevelThreeAndOuttake());
-    button4.whileTrue(elevator.jogUpCommand());
-    button6.whileTrue(elevator.jogDownCommand());
-    button7.whileTrue(outtake.jogForwardCommand());
-    button9.whileTrue(outtake.jogBackwardCommand());
+  public Robot() {
+    m_robotContainer = new RobotContainer();
   }
 
   @Override
   public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
+    CommandScheduler.getInstance().run(); 
   }
 
   @Override
-  public void disabledInit() {
-  }
+  public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {
-  }
+  public void disabledPeriodic() {}
 
   @Override
-  public void disabledExit() {
-  }
+  public void disabledExit() {}
 
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
   @Override
-  public void autonomousPeriodic() {
-  }
+  public void autonomousPeriodic() {}
 
   @Override
-  public void autonomousExit() {
-  }
+  public void autonomousExit() {}
 
   @Override
   public void teleopInit() {
@@ -87,12 +54,10 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {
-  }
+  public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {
-  }
+  public void teleopExit() {}
 
   @Override
   public void testInit() {
@@ -100,10 +65,11 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void testPeriodic() {
-  }
+  public void testPeriodic() {}
 
   @Override
-  public void testExit() {
-  }
+  public void testExit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
